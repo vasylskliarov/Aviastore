5b31393486508d9112028d20f119b828db7b7f74
vasylskliarov <vasylskliarov@gmail.com>
2014-05-07 22:57:19 +0300

    Added 3 beans
    AnalystBean, LoginBean, SecurityBean

--- src/main/java/com/aviastore/beans/AnalystBean.java
+++ src/main/java/com/aviastore/beans/AnalystBean.java
@@ -1,11 +1,15 @@
 package com.aviastore.beans;
 
 import java.io.Serializable;
+import java.util.*;
 
+import org.primefaces.model.chart.CartesianChartModel;
+import org.primefaces.model.chart.ChartSeries;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import com.aviastore.entitys.*;
 import com.aviastore.services.impl.*;
 
 @Component
@@ -18,8 +22,156 @@ public class AnalystBean implements Serializable {
 	@Autowired 
 	OrdersServicesImpl ordersServices;
 
+	private Date startDate;
+	private Date endDate;
+
+	private List<Report> repByDateList;
+	private List<Report> repByPlaceList;
+
+	private boolean showByDate = false;
+	private boolean showByPlace = false;
+
+	private CartesianChartModel modelByDate = new CartesianChartModel();
+	private CartesianChartModel modelByPlace;
 	
 	public AnalystBean() {}
+	
+	public void generateReports() {
+		this.generateReportByDate();
+		this.generateReportByPlace();
+		if (repByDateList.size() > 0){
+			this.showByDate = true;
+			this.createModelByDate();
+			this.createModelByPlace();
+		}
+	}
+
+	private void createModelByPlace() {
+		System.out.println("!!!modelPlaceCreating!!!");
+		modelByPlace = new CartesianChartModel();
+		ChartSeries quantitySeries = new ChartSeries();
+		quantitySeries.setLabel("Quantities");
+		for (Report rp :repByPlaceList) {
+			quantitySeries.set(rp.getOrigin()+"-"+rp.getDestination(), rp.getQuantity());
+		}
+		ChartSeries priceSeries = new ChartSeries();
+		priceSeries.setLabel("Price");
+		for (Report rp : repByPlaceList) {
+			priceSeries.set(rp.getOrigin()+"-"+rp.getDestination(), rp.getTotal());
+		}
+		modelByPlace.addSeries(priceSeries);
+		modelByPlace.addSeries(quantitySeries);	
+	}
+
+	private void createModelByDate() {
+		System.out.println("~~modelCreating~~");
+		modelByDate = new CartesianChartModel();
+		ChartSeries quantitySeries = new ChartSeries();
+		quantitySeries.setLabel("Quantities");
+		for (Report rp : repByDateList) {
+			quantitySeries.set(rp.getDate(), rp.getQuantity());
+		}
+		ChartSeries priceSeries = new ChartSeries();
+		priceSeries.setLabel("Price");
+		for (Report rp : repByDateList) {
+			priceSeries.set(rp.getDate(), rp.getTotal());
+		}
+		modelByDate.addSeries(priceSeries);
+		modelByDate.addSeries(quantitySeries);
+	}
+	
+	public void generateReportByDate() {
+		if (startDate == null || endDate == null) {
+			return;
+		}
+		this.repByDateList = ordersServices.getStatByDates(startDate, endDate);
+	}
+
+	public void generateReportByPlace() {
+		if (startDate == null || endDate == null) {
+			return;
+		}
+		this.repByPlaceList = ordersServices.getStatByCitys(startDate, endDate);
+	}
+
+	public OrdersServicesImpl getOrdersServices() {
+		return ordersServices;
+	}
+
+	public void setOrdersServices(OrdersServicesImpl ordersServices) {
+		this.ordersServices = ordersServices;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public List<Report> getRepByDateList() {
+		return repByDateList;
+	}
+
+	public void setRepByDateList(List<Report> repByDateList) {
+		this.repByDateList = repByDateList;
+	}
+
+	public List<Report> getRepByPlaceList() {
+		return repByPlaceList;
+	}
+
+	public void setRepByPlaceList(List<Report> repByPlaceList) {
+		this.repByPlaceList = repByPlaceList;
+	}
+
+	public boolean isShowByDate() {
+		return showByDate;
+	}
+
+	public void setShowByDate(boolean showByDate) {
+		this.showByDate = showByDate;
+	}
+
+	public boolean isShowByPlace() {
+		return showByPlace;
+	}
+
+	public void setShowByPlace(boolean showByPlace) {
+		this.showByPlace = showByPlace;
+	}
+
+	public CartesianChartModel getModelByDate() {
+		return modelByDate;
+	}
+
+	public void setModelByDate(CartesianChartModel modelByDate) {
+		this.modelByDate = modelByDate;
+	}
+
+	public CartesianChartModel getModelByPlace() {
+		return modelByPlace;
+	}
+
+	public void setModelByPlace(CartesianChartModel modelByPlace) {
+		this.modelByPlace = modelByPlace;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+	
 	
 	
 
--- src/main/java/com/aviastore/beans/LoginBean.java
+++ src/main/java/com/aviastore/beans/LoginBean.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import com.aviastore.services.impl.UsersServicesImpl;
+import com.aviastore.services.impl.*;
+import com.aviastore.entitys.*;
+
 
 @Component
 @Scope("session")
@@ -17,10 +19,45 @@ public class LoginBean implements Serializable {
 	//TODO in LoginBean try to use interface usersServices
 	@Autowired
 	private UsersServicesImpl usersServices;
-
+	public UsersServicesImpl getUsersServices() {
+		return usersServices;
+	}
+	public void setUsersServices(UsersServicesImpl usersServices) {
+		this.usersServices = usersServices;
+	}
+	
+	private String login;
+	private String password;
 	
 	public LoginBean() {}
-	
+	public String getLink() {
+		int role = usersServices.isValid(login, password);
+		if (role != -1) {
+			switch (role) {
+			case Users.ACCOUNTANT: return "accountant?faces-redirect=true";
+			case Users.ADMINISTRATOR: return "administrator?faces-redirect=true";
+			case Users.ANALYTIC: return "analytic?faces-redirect=true";
+			case Users.BLOCKED:	return "blocked?faces-redirect=true";
+			case Users.SECURITY_OFFICER: return "security?faces-redirect=true";
+			}
+		}
+		return "login";
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 	
 
 }
